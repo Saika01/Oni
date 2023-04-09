fbcbd322b049a79a9a0a0039473298d826f2ac99
Aoi <naztya2001@bk.ru>
2023-04-09 13:13:35 +0300

    made a function creating the array

--- TheTask/Program.cs
+++ TheTask/Program.cs
@@ -1,6 +1,16 @@
-﻿string[] array = new string[3];
+﻿string[] array = new string[4];
 
 array[0] = "nose";
 array[1] = "see";
 array[2] = "enorme";
+array[3] = "bo";
 
+string[] CreateArrayThreeSymbolsLess(string[] arr)
+{
+    string[] newArr = new string[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <=3) newArr[i] = arr[i];
+    }
+    return newArr;
+}
